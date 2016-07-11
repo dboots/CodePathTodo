074399d31885c6bdad81beb47aa4f2b372911b18
dboots <don@donboots.com>
2016-07-11 16:11:25 -0400

    import cleanup

--- app/src/main/java/com/donboots/codepathtodo/MainActivity.java
+++ app/src/main/java/com/donboots/codepathtodo/MainActivity.java
@@ -7,7 +7,6 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
 
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
